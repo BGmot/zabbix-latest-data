5336e0d07e71a537e5509338c302eb19dbb5e80b
Evgeny Yurchenko <ey@bgmot.com>
2021-07-18 20:22:52 -0400

    latest-data-patch.sh updated to 5.4.1.

--- latest-data-patch.sh
+++ latest-data-patch.sh
@@ -6,7 +6,7 @@ set -o
 
 ZABBIX_INSTALL_PATH=/usr/share/zabbix
 
-VERSION=5.4.0
+VERSION=5.4.1
 ZABBIX_INSTALLED_VERSION=`grep ZABBIX_VERSION ${ZABBIX_INSTALL_PATH}/include/defines.inc.php | cut -d\' -f 4`
 
 if [ "$VERSION" != "$ZABBIX_INSTALLED_VERSION" ]
